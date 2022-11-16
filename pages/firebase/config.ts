631929cd9342230fbf8eb914592eb0387b67a669
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2022-11-16 09:43:19 -0300

    tudo menos o firebase

--- pages/firebase/config.ts
+++ pages/firebase/config.ts
@@ -1,1 +1,10 @@
 import firebase from "@firebase/app-compat";
+import firestore from "firebase/compat";
+if (!firebase.apps.length){
+apikey:process.env. NEXT_PUBLIC_apiKey,
+    authDomain:process.env,
+        ProjectId:process.env.
+
+}
+
+export default firebase
