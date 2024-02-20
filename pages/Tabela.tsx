acdf4dee4e7b17653faa042e5fe45d4d766510c0
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-20 09:40:47 -0300

    revisado

--- pages/Tabela.tsx
+++
@@ -1,9 +0,0 @@
-
-import Cliente from "./Cliente"
-import Botoes from "./Botoes"
-import Botaoeditar from "./Botoes"
-import React from "react"
-import Titulo from "./Titulo"
- export default function Tabela (){
-return <table><Titulo/> <tr><th></th><Botaoeditar/><th></th></tr><tr></tr><tr>dados<td><Cliente/></td></tr><tr><td>campos</td><td>id</td><td>idade</td><td>nome</td></tr></table>
- }
