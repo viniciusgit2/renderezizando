03ff5ff37ca2cb335455d8fee41ea5ee29c2c72f
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-20 11:42:17 -0300

    com fundamentos como comunição direta

---
+++ components/comunicacao/direta/Filho.jsx
@@ -0,0 +1,11 @@
+export default function Filho(props){
+    return(
+    <div>
+        <h2>
+            {props.familia}
+            
+        </h2>
+        
+    </div>
+    )
+}
---
+++ pages/comunicacao/Direta.jsx
@@ -0,0 +1,4 @@
+import Pai from "../../components/comunicacao/direta/Pai"
+export default function direta(props){
+    return (<div><h1><Pai/></h1></div>)  
+  }
