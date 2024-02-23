523c0b0a79ba0b435a428072e9fcf6aad0e4ba5b
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-23 08:21:54 -0300

    com contador

---
+++ pages/estado/contador.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+export default function contador(){
+    const [num,setnumero]=useState(0)
+    const soma=()=>{
+setnumero(num+1)
+    }
+    const diminuir=()=>{
+        setnumero(num-1)
+
+    }
+    return (<div><h1>contador</h1><div><button onClick={soma}>+</button><div><button onClick={diminuir}>-</button></div>{num}</div><div>valor:{num}</div></div>)
+}
