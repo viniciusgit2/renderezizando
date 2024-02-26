4cb99cc81af79c9148426fbfb92f1af25fb5834c
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-26 10:26:18 -0300

    com tsx

---
+++ components/Pessoa.tsx
@@ -0,0 +1,9 @@
+
+interface Pessoaprops{
+nome:string
+idade:number
+}
+export  function Pessoa(props:Pessoaprops){
+
+    return (<div><div>nome{props.nome}</div><div>idade{props.idade}</div></div>)
+}
