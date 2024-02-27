c601eb79fae001c717eab7b50ea06c3fca0702cd
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-27 10:06:59 -0300

    com um buscar

--- pages/Desafio.jsx
+++ pages/Desafio.jsx
@@ -1,4 +1,4 @@
-export default function desafio(){
+export default function Desafio(){
     return (
 <div>
     <span>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import Subdivisao from './tabuleiro/componentes/Subdivisao'
 import styles from '../styles/Home.module.css'
 export default function Home() {
