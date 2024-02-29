2a970bfa2b3e40f215f41103bd1ccbc4624d5836
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-29 10:44:48 -0300

    começando o jogo de porta

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import mega from "../pages/mega/mega";
 import metodo from "../pages/api/metodo";
+import Presente from '../components/Presente'
 export default function Home() {
   return (
     <div>
       <Link href="mega/mega">megacena</Link>
       <Link href="estado/contador">,contador</Link>
       <Link href="api/metodo">,metodo</Link>
+      <Link href='components/Presente'></Link>
     </div>
   );
 }
