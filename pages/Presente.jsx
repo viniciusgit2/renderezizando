dcf49a5b4c27a24ac5b1ba12712f49f6af4d0d3d
viniciusgit2 <4xv6patelfin@gmail.com>
2024-02-29 11:56:20 -0300

    ajustando o css

--- pages/Presente.jsx
+++ pages/Presente.jsx
@@ -1,5 +1,6 @@
 export default function Presente() {
-    return (<div className="presente">teste</div>
+    return (<div className="presente"> <div className="tampa"></div><div className="corpo"></div><div className="laco1"></div><div className="laco2"></div></div>
+   
     )
 }
     
