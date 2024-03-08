c8c29c044975d96cb050625104371f064803d624
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-08 10:47:09 -0300

    agora sim estatico

--- pages/Estatico1.jsx
+++ pages/Estatico1.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react"
 
 export default function estatico1(){
     const [num,setnum]=useState(0)
     useEffect(()=>{setnum(Math.random())},{})
   return(<div><h1>new estatico</h1>
-  <h2>{num}</h2>
+
   </div>)    
 }
