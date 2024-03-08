575166e077b0e86be644d68df45c30b2f362c3cf
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-08 11:24:29 -0300

    corrigido outro bug

--- pages/Estatico2.jsx
+++ pages/Estatico2.jsx
@@ -1,12 +1,12 @@
-export function getstatic(){
+export function getStaticProps(){
 return {
     props:{
-        numero:Math.random()
+        numero : Math.random()
     }
 }
 
 }
-export default function estatico2(props){
+export default function Estatico2(props){
   return(<div><h1> estatico</h1>
     <h2>{props.numero}</h2>
   </div>)    
