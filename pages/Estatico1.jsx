d0bfb126075b6e7383a419b77ef496c8f064f3a0
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-08 10:47:34 -0300

    agora sim estatico

--- pages/Estatico1.jsx
+++ pages/Estatico1.jsx
@@ -2,7 +2,7 @@
 export default function estatico1(){
     const [num,setnum]=useState(0)
     useEffect(()=>{setnum(Math.random())},{})
-  return(<div><h1>new estatico</h1>
+  return(<div><h1> estatico</h1>
 
   </div>)    
 }
