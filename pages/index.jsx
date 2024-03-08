f32df212787a2df3dc2de83a74d7e9682ccec42e
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-08 10:44:35 -0300

    com estatico

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import mega from "../pages/mega/mega";
 import Presente from "./Presente";
 import Porta from "./Porta";
 import { useState } from "react";
+import Estatico1 from "./Estatico1";
 import Subdivisao from '../pages/tabuleiro/componentes/Subdivisao'
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
       <Link href="api/metodo">,metodo</Link>
       <Link href='mega/mega'>,megacena</Link>
       <Link href='estado/Contador'>,contador</Link>
-       <Link href='tabuleiro/componentes/Subdivisao'>tabuleiro</Link>
+       <Link href='tabuleiro/componentes/Subdivisao'>,tabuleiro</Link>
+       <Link href='Estatico1'>,estatico1</Link>
       <Porta />   
     
 
