d2613c215a5f7091959466d78e19563b2f3068d7
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-12 11:49:06 -0300

    ajustado o dinamico2

--- pages/Dinamico2.jsx
+++ pages/Dinamico2.jsx
@@ -2,7 +2,7 @@ export async function getStaticProps(){
     const resp=await fetch("http://localhost:3000/api/produtos")//repare o localhost o numero é variavel pra funcionar ai varia a ordem pra ir
     //e com api aberta mas esse é o ideal 
     console.log('{server....} gerando props para o componente ...')//teste para ver se no server gerava ou não
-     const produtos=await resp.json()
+     const produtos= await resp.json()
      return{
          props:{
              produtos
