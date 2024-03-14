7c8989f697ed27d644113ff1c5c6e28b7cf8e20f
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-14 09:47:16 -0300

    com id feito

---
+++ pages/alunos/[id].jsx
@@ -0,0 +1,20 @@
+
+export function getStaticPaths(){
+return{
+  fallback:false,//404
+  paths:[
+    {
+    params:{id:`107`},
+    params:{id:`203`},
+    params:{id:`1345`}
+    }
+  ]
+}
+  
+}
+export async function getStaticProps(context){
+
+}
+export default function id(){
+
+}
---
+++ pages/api/alunos/[id].js
@@ -0,0 +1,9 @@
+export default function handler(req,res){
+    const id=+req.query.id
+    res.status(200).json({
+    id:123,
+    nome:`status`
+    
+})
+    
+}
