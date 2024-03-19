d67ba5a8ced0dd6a6d541be5d1f4dbfbaafa5916
viniciusgit2 <4xv6patelfin@gmail.com>
2024-03-19 09:22:59 -0300

    problema com id

--- pages/alunos/[id].jsx
+++ pages/alunos/[id].jsx
@@ -1,10 +1,23 @@
 export async function getStaticPaths(){
-
-  const  resp = await fetch(`http://localhost:3000/api/alunos/tutores`)
-  const ids =await resp.json()
-  const paths=ids.map(id=>{return {params: {id}}})
-}
-return {
-  fallback:false,//404
-}
-
+  return {
+    fallback:false,//404
+    paths:[
+      {params: {id: `107`}},//(404 na pratica pois impossivel aceetar a porta)
+      {params: {id: `203`}},
+      {params: {id: `1345`}},
+  
+    ]
+  
+    }
+  
+  
+    }
+    export async function getStaticProps(context){
+      const resp=await(`http://localhost:3000/api/alunos/123/${context.params.id}`)
+      return {
+        props:{}
+      }
+    }
+    export default function aluno(){
+return(<div>oi</div>)
+    }
